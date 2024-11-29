cbe149e684763860544aea9c9b5ce0d6cfc9b156
Obijuru <158084677+Obijuru@users.noreply.github.com>
2024-11-29 11:42:39 +0100

    add z-index

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,7 +2,7 @@ import LayersClearIcon from '@mui/icons-material/LayersClear';
 
 const Header = () => {
   return (
-    <div className="bg-yellow-500 fixed p-4 top-0 right-0 left-0">
+    <div className="bg-yellow-500 p-4 z-10 fixed top-0 right-0 left-0">
       <div className="ml-2 font-montserrat font-bold text-white"><LayersClearIcon /> Keeper</div>
     </div>
   )
