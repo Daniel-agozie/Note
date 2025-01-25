41e5d0078cdd1ff99837a5d6333a71d596a73611
Obijuru <158084677+Obijuru@users.noreply.github.com>
2025-01-25 12:51:12 +0100

    updated version

--- src/CreateNote.jsx
+++ src/CreateNote.jsx
@@ -9,7 +9,7 @@ const CreateNote = () => {
     <div className='mt-5 ml-3 p-4 gap-10 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 mr-4'>
       {holdNote.map((hold,index) => {
         return(
-          <div className='border-2 p-3 break-words relative w-64 h-32 overflow-y-scroll scrollbar-hide shadow-lg ' key={index} id={index}>
+          <div className='border-2 p-3 break-words relative h-32 overflow-y-scroll scrollbar-hide shadow-lg ' key={index} id={index}>
             <div>{hold.title}</div>
             <div>{hold.content}</div>
 
